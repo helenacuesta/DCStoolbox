17430d8c95c73f1233d5d059a90603c84a5dc838
Helena Cuesta <helenacuesta@testingappinventor.home>
2019-02-12 08:44:59 +0100

    edit cutting script to leave output filename blank

--- cut_audio_files.py
+++ cut_audio_files.py
@@ -6,17 +6,23 @@ import sox
 import pandas as pd
 import numpy as np
 
-def cut_audio_iteratively(input_file,bounds,output_path):
+def cut_audiofile_iteratively(input_file,bounds,output_path):
 
     cnt=0
+    # cut one segment of the input audio at each iteration of the for loop
     for line in bounds:
 
         cnt+=1
+        # define time boundaries of each segment
         tin,tout = line[0],line[1]
 
         tfm = sox.Transformer()
         tfm.trim(tin,tout)
-        output_file = output_path + '{}_{}_{}_{}.wav'.format(line[2],line[3],line[4],line[5])
+
+        #output_file = output_path + '{}_{}_{}_{}.wav'.format(line[2],line[3],line[4],line[5])
+
+        # TODO: we need to define how to name the files automatically given the csv files with annotations
+        output_file = ''
 
         tfm.build(input_file,output_file)
 
@@ -29,14 +35,13 @@ def read_time_boundaries(csvfile):
 
     return bounds
 
-### Calling stuff here
+### Calling stuff here. In the final code this should iterate over all the audio tracks
 
 bounds_file = '../cut_annotations_v1.csv'
 audio_file = '../audioInput/Dagstuhl_Stereo.wav'
 
 bounds = read_time_boundaries(bounds_file)
-
-cut_audio_iteratively(audio_file,bounds,'../audioOutput/')
+cut_audiofile_iteratively(audio_file,bounds,'../audioOutput/')
 
 
 
