f8093cdfca50b2f0e043f3498db1ca4ab08925f7
Sebastian Rosenzweig <sebastian.rosenzweig@audiolabs-erlangen.de>
2019-02-22 16:23:06 +0100

    improved cutting, still buggy...

--- sync_audio_video.py
+++ sync_audio_video.py
@@ -10,6 +10,8 @@ import numpy as np
 import ffmpeg
 import os
 import subprocess
+from multiprocessing import Pool
+import time
 
 # set paths
 video_path = './data/'
@@ -36,44 +38,57 @@ def compute_sync_lag(s1, s2):
 
 
 def extract_audio_from_video(video, fs):
+    time_ms = int(round(time.time() * 1000))
     command = ffmpeg.input(video)
-    command = ffmpeg.output(command, "temp.wav")
+    command = ffmpeg.output(command, str(time_ms) + '.wav')
     ffmpeg.run(command)
 
-    video_audio, _ = librosa.load('temp.wav', sr=fs)
-    os.remove('temp.wav')
+    video_audio, _ = librosa.load(str(time_ms) +'.wav', sr=fs)
+    os.remove(str(time_ms) + '.wav')
 
     return video_audio
 
 
-def cut_and_save_video(video, start, duration, output_path):
-    video_path = os.path.abspath(output_path + os.path.basename(video[:-3])) + 'mp4'
-    command = "ffmpeg -ss " + str(start) + " -i " + os.path.abspath(video) + " -vcodec libx264 -t " + str(duration) + " " + video_path
+def cut_and_save_video(video, start, duration, output_file):
+    command = "ffmpeg -ss " + str(start) + " -i " + os.path.abspath(video) + " -vcodec libx264 -t " + str(duration) + " " + output_file
 
     subprocess.call(command, shell=True)
 
 
 # loop through all video files available
-for video_file in file_list:
+def processing_wrapper(video_file):
     print('Current file: ' + video_file)
 
     # extract video audio
     video_audio = extract_audio_from_video(video_file, fs)
 
-    # compute crosscorrelation with referencedata
+    # compute crosscorrelation with reference audio
     lag = compute_sync_lag(audio_ref, video_audio)
-    start_time_s = lag/fs
-    end_time_s = (lag + len(video_audio))/fs
+    start_ref_s = lag/fs  # start in reference audio
+    end_ref_s = (lag + len(video_audio))/fs  # end in reference audio
 
     # cut video according to cut annotation
-    cut_anno_sorted =  cut_anno.iloc[(cut_anno['Start Time (sec)'] - start_time_s).abs().argsort()].to_numpy()  # find closest value to calculated lag
-    actual_start_s = cut_anno_sorted[0, 0]
-    actual_end_s = cut_anno_sorted[0, 1]
-    cut_start_s = actual_start_s - start_time_s
-    cut_end_s = actual_end_s - start_time_s
+    # loop through whole video and find all cutpoints
+    search_start_s = start_ref_s
+    while 1:
+        cut_anno_sorted = cut_anno.iloc[(cut_anno['Start Time (sec)'] - search_start_s).abs().argsort()].to_numpy()  # find closest value to calculated lag
+        actual_start_s = cut_anno_sorted[0, 0]
+        actual_end_s = cut_anno_sorted[0, 1]
 
-    cut_and_save_video(video_file, cut_start_s, cut_end_s-cut_start_s, './output/')
+        cut_start_s = actual_start_s - start_ref_s
+        cut_end_s = actual_end_s - start_ref_s
 
+        if cut_end_s > len(video_audio)/fs:
+            # break if snippet is not fully contained in video file
+            break
 
-    # save annotation
-    pd.DataFrame([os.path.basename(video_file), start_time_s, end_time_s]).T.to_csv(output_anno_path, header=False, mode='a', index=False)
+        cur_out_file = './output/' + cut_anno_sorted[0, 2] + '_' + cut_anno_sorted[0, 3] + '_' + cut_anno_sorted[0, 4] + '_SATB_Video.mp4'
+        cut_and_save_video(video_file, cut_start_s, cut_end_s-cut_start_s, cur_out_file)
+
+        # save annotation
+        pd.DataFrame([os.path.basename(video_file), start_ref_s, end_ref_s]).T.to_csv(output_anno_path, header=False, mode='a', index=False)
+
+        search_start_s = actual_end_s
+
+p = Pool(processes=None)
+p.map(processing_wrapper, np.sort(file_list))
