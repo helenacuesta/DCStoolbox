08ce55deb28b03ea82377cf8cc431a5c040f93a2
helenacuesta <hcuesta.8@gmail.com>
2019-11-14 11:41:06 +0100

    forgot to edit one script

--- choirset_toolbox/preparation/normalize_downsample.py
+++ choirset_toolbox/preparation/normalize_downsample.py
@@ -2,9 +2,9 @@
 This scrip uses PySox to normalize audiofiles in a folder to -2dB and downsample them (if necessary) to 22050 Hz.
 '''
 
-import utils
 import os
-from utils.data_prep import config
+from choirset_toolbox.preparation import utils
+from choirset_toolbox.preparation import config
 
 for filename in os.listdir(config.output_path):
 
