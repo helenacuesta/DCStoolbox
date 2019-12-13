fc1fedc1a72c0fe1eb88a18ef62575cec7dcc48a
sebastianrosenzweig <basti.rosenzweig@googlemail.com>
2019-12-13 16:20:06 +0100

    updated parser to new file naming/folder structure, sonification function

--- DCStoolbox/main.py
+++ DCStoolbox/main.py
@@ -15,7 +15,7 @@ class Error(Exception):
     """Simple error class"""
     pass
 
-def DCS_content_parser(DCS_path='./data/DagstuhlChoirSet/', song_id='*', setting='*', take='*', section='*', mic='*'):
+def DCS_content_parser(DCS_path='./DagstuhlChoirSet_V1.0/', song_id='*', setting='*', take='*', section='*', mic='*'):
     """Function for parsing dataset content.
 
     Args:
@@ -33,35 +33,33 @@ def DCS_content_parser(DCS_path='./data/DagstuhlChoirSet/', song_id='*', setting
     if not os.path.exists(DCS_path):
         raise Error('Invalid path to Dagstuhl Choir Set!')
 
-    if song_id not in ['*', 'DLI', 'DTP', 'DSE']:
+    if song_id not in ['*', 'DCS_LI', 'DCS_TP', 'DCS_SE']:
         raise Error('Song-ID not valid!')
 
-    if setting not in ['*', 'All', 'QuartetA', 'QuartetB', 'Basses']:
+    if setting not in ['*', 'FullChoir', 'QuartetA', 'QuartetB', 'Basses']:
         raise Error('Setting-ID not valid!')
 
-    section_list = ['Stereo', 'S1', 'S2', 'A1', 'A2', 'T1', 'T2', 'B1', 'B2', 'B3', 'B4', 'B5', 'Piano', '*']
+    section_list = ['Stereo', 'StereoReverb', 'S1', 'S2', 'A1', 'A2', 'T1', 'T2', 'B1', 'B2', 'B3', 'B4', 'B5', 'Piano', '*']
     if section not in section_list:
         raise Error('Section-ID not valid!')
 
     if mic not in ['*', 'STM', 'STL', 'STR', 'SPL', 'SPR', 'DYN', 'HSM', 'LRX']:
         raise Error('Microphone-ID not valid!')
 
-    # set paths to subfolders ([Name, Path, Suffix, File-Extension])
-    path_spec = np.array([['Audio', DCS_path + 'Audio/norm-downsampled/', '', 'wav'],
-                          ['BeatAnnotation', DCS_path + 'Annotations/beat_alabs/', '_beatAnnotations', 'csv'],
-                          ['MIDI', DCS_path + 'Annotations/SheetMusic_aligned_AudioLabsBeats/', '', 'midi'],
-                          ['MIDIsoni', DCS_path + 'Annotations/SheetMusic_aligned_AudioLabsBeats/', '','wav'],
-                          ['F0CREPE', DCS_path + 'Annotations/pitch/CREPE/', '', 'csv'],
-                          ['F0PYIN', DCS_path + 'Annotations/pitch/PYIN/modified_unvoiced/', '', 'csv']])
+    # set paths to subfolders ([Name, Path, File-Extension])
+    path_spec = np.array([['Audio', DCS_path + 'audio_wav_22050_mono/', 'wav'],
+                          ['Beat', DCS_path + 'annotations_csv_beat/', 'csv'],
+                          ['ScoreRepr', DCS_path + 'annotations_csv_scorerepresentation/', 'csv'],
+                          ['F0CREPE', DCS_path + 'annotations_csv_F0_CREPE/', 'csv'],
+                          ['F0PYIN', DCS_path + 'annotations_csv_F0_PYIN/', 'csv']])
 
     # initialize new Pandas dataframe
-    table_headers = ['Song-ID', 'Setting', 'Take', 'Section', 'Microphone']
+    table_headers = ['Dataset-ID', 'Song-ID', 'Setting', 'Take', 'Section', 'Microphone']
     table_headers.extend(path_spec[:, 0])
     DCS_table = pd.DataFrame(columns=table_headers)
 
     # fill DCS_table
-    audio_list = glob.glob('%s%s_%s_%s_%s_%s%s.%s'%(path_spec[0, 1], song_id, setting, take, section, mic, path_spec[0, 2],
-                                                    path_spec[0, 3]))
+    audio_list = glob.glob('%s%s_%s_%s_%s_%s.%s'%(path_spec[0, 1], song_id, setting, take, section, mic, path_spec[0, 2]))
 
     for audio_file in audio_list:
         new_row = []
@@ -72,19 +70,24 @@ def DCS_content_parser(DCS_path='./data/DagstuhlChoirSet/', song_id='*', setting
         new_row.extend(tail_s)
         new_row.append(audio_file)
 
-        cur_song_id = tail_s[0]
-        cur_setting = tail_s[1]
-        cur_take = tail_s[2]
-        cur_section = tail_s[3]
-        cur_mic = tail_s[4]
+        cur_dataset_id = tail_s[0]
+        cur_song_id = tail_s[1]
+        cur_setting = tail_s[2]
+        cur_take = tail_s[3]
+        cur_section = tail_s[4]
+        cur_mic = tail_s[5]
 
         # go through available annotations
         for data_idx in range(1, path_spec.shape[0]):
-            file_path = '%s%s_%s_%s_%s_%s%s.%s'%(path_spec[data_idx, 1], cur_song_id, cur_setting, cur_take,
-                                                 cur_section, cur_mic, path_spec[data_idx, 2], path_spec[data_idx, 3])
+            file_path = '%s%s_%s_%s_%s_%s_%s*.%s'%(path_spec[data_idx, 1], cur_dataset_id, cur_song_id, cur_setting, cur_take,
+                                                   cur_section, cur_mic, path_spec[data_idx, 2])
+            file_list = glob.glob(file_path)
 
-            if os.path.exists(file_path):
-                new_row.append(file_path)
+            if file_list:
+                if len(file_list) == 1:
+                    new_row.append(file_list[0])
+                else:
+                    new_row.append(tuple(file_list))
             else:
                 # no files found
                 new_row.append('')
@@ -175,4 +178,38 @@ def sonify_trajectory_with_sinusoid(traj, audio_len, Fs=22050, amplitude=0.3, sm
     # filter amplitudes to avoid transients
     amplitude_mod = np.convolve(amplitude_mod, np.hanning(smooth_len)/np.sum(np.hanning(smooth_len)), 'same')
     x_soni = x_soni * amplitude_mod
+    return x_soni
+
+
+def sonify_score_representation_with_sinusoidals(fns_score, Fs, audio_len_samples):
+    """Sonification of score representation
+
+    Args:
+        fns_score: Tuple of paths to score representation files
+        Fs: Sampling rate
+        audio_len_samples: Desired audio length in samples
+
+    Returns:
+        x_soni: Sonification
+    """
+    H = 256
+    t = np.arange(0, audio_len_samples/Fs, H/Fs)
+    x_soni = np.zeros(audio_len_samples)
+
+    for fn in fns_score:
+        score_rep = read_csv(fn, header=None).values
+
+        traj = np.hstack((t.reshape(-1, 1), np.zeros(len(t)).reshape(-1,1)))
+
+        for row in score_rep:
+            # get indices of trajectory
+            t_start_idx = np.argmin(np.abs(t - row[0]))
+            t_end_idx = np.argmin(np.abs(t - row[1]))
+            note_len_idx = t_end_idx - t_start_idx
+
+            f_hz = 440 * 2 ** ((row[2]-69)/12)
+            traj[t_start_idx:t_end_idx, 1] = [f_hz]*note_len_idx
+
+        x_soni += sonify_trajectory_with_sinusoid(traj, audio_len_samples, Fs=Fs, amplitude=0.3, smooth_len=11)
+
     return x_soni
